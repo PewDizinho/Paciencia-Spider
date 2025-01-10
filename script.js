82151686491def58cb40c38017373a1590637ada
PewDizinho <konopkspaulo046@gmail.com>
2025-01-10 14:47:32 -0300

    Refactor card handling and update CSS for responsive design

--- script.js
+++ script.js
@@ -27,15 +27,10 @@ document.addEventListener("drop", function (event) {
     event.preventDefault();
     if (event.target.parentNode.className == "row") {
         const data = event.dataTransfer.getData("Id");
-
-        
-        console.log(event.target.id.split("-")[0] + " " + ((new Number(data.split("-")[0])) + 1))
-        if (event.target.id.split("-")[0] == ((new Number(data.split("-")[0])) + 1) || event.target.classList[1] == "inv"){
+        if (event.target.id.split("-")[0] == ((new Number(data.split("-")[0])) + 1) || event.target.classList[1] == "inv") {
             event.target.parentNode.appendChild(new Card({ number: data.split("-")[0], cardType: data.split("-")[1] }).getObj());
             document.getElementById(event.dataTransfer.getData("OriginalPlace")).querySelector(`[id='${data}']`).remove();
         }
-        
-        // event.dataTransfer.getData("teste").remove();
 
     }
 });
@@ -52,27 +47,34 @@ class Card {
 
         for (var i = 1; i <= 10; i++) {
             for (let j = 0; j <= 4; j++) {
-                if (Math.floor(Math.random() * 10 < 5)) { continue; }
-                document.getElementById(`row${i}`).appendChild(this.pickRandomCard().getObj());
+                if (Math.floor(Math.random() * 10 < 7)) { continue; }
+                var pick = this.pickRandomCard();
+                if (!pick) return;
+                document.getElementById(`row${i}`).appendChild(pick.getObj());
             }
         }
     }
 
     pickRandomCard() {
         {
-            //justBlack
-            let randomEspadaNumber = Math.floor(Math.random() * cards.espada.length);
-            cards.espada.slice(randomEspadaNumber, 1);
-            return new Card({ number: cards.espada[randomEspadaNumber], cardType: "espada" });
+            // justBlack
+            if (cards.espada.length > 0) {
+                let randomEspadaNumber = Math.floor(Math.random() * cards.espada.length);
+                console.log(randomEspadaNumber);
+                var result = new Card({ number: cards.espada[randomEspadaNumber], cardType: "espada" });
+                cards.espada.splice(randomEspadaNumber, 1);
+                return result;
+            } else {
+                return false;
+            }
+
         }
     }
     getCardSrc() {
         if (["copas", "espada", "ouro", "paus"].findIndex((element) => element == this.cardType) >= 0 && ((this.number > 0 && this.number < 14))) {
             return `./assets/cards/${this.cardType}/${this.number}.svg`
         }
-        this.number = 1;
-        this.cardType = "copas";
-        throw "Carta inválida";
+        throw "Carta inválida: " + this.number + "-" + this.cardType;
     }
 
     getObj() {
